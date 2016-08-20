f11bd88e2eceef6e0cff0fe379d02feb97d5f437
Yitzhak <yitzhak@razorgrip.com>
2016-08-21 00:17:17 +0300

    Start the sidebar

--- src/app.js
+++ src/app.js
@@ -29,9 +29,28 @@ const store = Redux.createStore(Redux.combineReducers({
     cards
 }));
 
+// pure build component function
 const App = (props) => {
     return (<div className="app">
         {props.children}
     </div>)
 };
-ReactDOM.render(<App>Hello <strong>React</strong> </App>, document.getElementById('root'));
+
+const Sidebar = React.createClass({
+    render(){
+        let props = this.props;
+        return (<div className="sidebar">
+            <h2>All Decks</h2>
+            <ul>
+                {props.decks.map((deck, i)=>
+                    <li key={i}>{deck.name}</li>
+                )}
+            </ul>
+            {props.addingDeck && <input ref="add" />}
+        </div>);
+    }
+});
+
+ReactDOM.render(<App>
+    <Sidebar decks={[{name:'deck 1'}]} addingDeck={true} />
+</App>, document.getElementById('root'));
